8c134116242811d0ffdff56592f2f8174441f276
pinguinjay <83256594+pinguinjay@users.noreply.github.com>
2022-11-22 19:45:38 +0800

    Create immuneMousetumor.r

---
+++ immuneMousetumor.r
@@ -0,0 +1,24 @@
+#  Null Hypothesis: The median difference between pairs of observations is zero
+#  Alternate Hypothesis: The median difference between pairs of observations is not zero
+#
+#    Alpha = 0.05
+#
+#####################################################################
+#
+tumorSize <- read.csv(file = "immuneMousetumor.csv", header = TRUE, sep = ",")
+print(tumorSize)
+#
+# Check for Normality
+shapiro.test(tumorSize$antiPDL1TumorGrowthRate)     # p < 0.05, data are not normal 非常態
+shapiro.test(tumorSize$PBSTumorGrowthRate)   # p > 0.05, data are normal 常態
+#
+# Perform Mann-Whitney Test using "wilcox.test()" function
+wilcox.test(tumorSize$antiPDL1TumorGrowthRate,tumorSize$PBSTumorGrowthRate, paired =  FALSE)
+#
+# Repeat test and swap variables
+wilcox.test(tumorSize$PBSTumorGrowthRate, tumorSize$antiPDL1TumorGrowthRate, paired = FALSE)
+#
+# Report: U = 23, p = 0.9015
+# Decision: Fail to reject Null Hypothesis
+# Conclusion: No difference in tumor groth rate between Males and Females
+#
