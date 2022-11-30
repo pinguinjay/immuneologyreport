c1a84a18f6bcdc1fd3cd4628255835a9d50d0654
pinguinjay <83256594+pinguinjay@users.noreply.github.com>
2022-11-30 23:03:22 +0800

    Create flow.r

---
+++ flow.r
@@ -0,0 +1,45 @@
+
+flow<-read.csv(file = "flow_1.csv", header = TRUE, sep = ",")
+flowdata<-flow[,-1]
+
+
+library(GGally)
+pm<-ggpairs(flowdata,mapping = aes(color = flowdata$treatmemt))
+pm
+
+
+library(tidyverse)
+PBS_flow<-filter(flowdata,flow$treatmemt=="PBS")
+PDL1_flow<-filter(flowdata,flow$treatmemt=="PDL1")
+#######計算細胞平均數#######
+ncols <- length(PDL1_flow)
+column_means <- vector(mode = "numeric", length = ncols)
+for (i in 1:ncols) {   
+  column_means[i] <- mean(PDL1_flow[[i]])
+}
+names(column_means) <- names(PDL1_flow)
+c<-column_means
+#
+ncols <- length(PBS_flow)
+column_means <- vector(mode = "numeric", length = ncols)
+for (i in 1:ncols) {   
+  column_means[i] <- mean(PBS_flow[[i]])
+}
+names(column_means) <- names(PBS_flow)
+column_means
+##########################
+
+wilcox.test(PBS_flow$Tc,PDL1_flow$Tc, paired =  FALSE)
+wilcox.test(PBS_flow$NK,PDL1_flow$NK,paired = FALSE)
+wilcox.test(PBS_flow$Neutrophil,PDL1_flow$Neutrophil, paired =  FALSE)
+wilcox.test(PBS_flow$NKT,PDL1_flow$NKT, paired =  FALSE)
+wilcox.test(PBS_flow$Th,PDL1_flow$Th, paired =  FALSE)
+wilcox.test(PBS_flow$CD3,PDL1_flow$CD3, paired =  FALSE)
+wilcox.test(PBS_flow$TcellInLiveCell,PDL1_flow$TcellInLiveCell, paired =  FALSE)
+wilcox.test(PBS_flow$TcellInImmuneCell,PDL1_flow$TcellInImmuneCell, paired =  FALSE)
+wilcox.test(PBS_flow$CD8OverCD3,PDL1_flow$CD8OverCD3, paired =  FALSE)
+wilcox.test(PBS_flow$CD8InLiveCell,PDL1_flow$CD8InLiveCell, paired =  FALSE)
+wilcox.test(PBS_flow$CD8InLiveCell,PDL1_flow$CD8InLiveCell, paired =  FALSE)
+wilcox.test(PBS_flow$Neutrophil/(PBS_flow$Tc+PBS_flow$Th+PBS_flow$NKT),PDL1_flow$Neutrophil/((PDL1_flow$Tc+PDL1_flow$Th+PDL1_flow$NKT)), paired =  FALSE)
+wilcox.test((PBS_flow$NK+PBS_flow$NKT)/PBS_flow$liveCell,(PDL1_flow$NK+PDL1_flow$NKT)/PDL1_flow$liveCell,paired = FALSE)
+mean()
